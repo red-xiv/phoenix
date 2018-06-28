81acb96f267d8649eda9eea531df2c5ea1ca45b2
James Johnstone <james.johnstone@geeks.ltd.uk>
2018-06-28 13:43:17 +0100

    modify image loader to use correct number of assets

--- js/image-loader.js
+++ js/image-loader.js
@@ -1,31 +1,44 @@
 class ImageLoader{
     loadAssets(){
-        if (!window.game)
-        throw new Error("you must load game class first..");
+		if (!window.game)
+			throw new Error("you must load game class first..");
+	
+		let numberOfHazards = 9;
+		let numberOfCoins = 6;
+		this.coins = [];
+		this.hazards = [];
 
-	this.coin = new Image();
-	this.hazards = [new Image(), new Image(), new Image()];
-
-    var numberOfHazards = 4;
-	var numImages = 1 + numberOfHazards;
-	var numLoaded = 0;
-
-	function imageLoaded() {
-		numLoaded++;
-		
-        if (numLoaded === numImages) {
-			window.game.init();
+		for (let i = 0; i < numberOfCoins; i++){
+			this.coins.push(new Image());
 		}
+
+		for (let i = 0; i < numberOfHazards; i++){
+			this.hazards.push(new Image());
+		}
+		let numImages = numberOfCoins + numberOfHazards;
+		let numLoaded = 0;
+
+		this.imageLoaded = () => {
+			numLoaded++;
+			
+			if (numLoaded === numImages) {
+				window.game.init();
+			}
+		}
+		
+		this.coin.onload = function() {
+			this.imageLoaded();
+		}
+		this.coin.src = "assets/coin.png";
+
+		this.hazards.forEach((h,i) => {
+			h.onload = () => imageLoaded();
+			h.src = `assets/coin (${i+1}).png`;
+		});
+		
+		this.hazards.forEach((h,i) => {
+			h.onload = () => imageLoaded();
+			h.src = `assets/hazzard (${i+1}).png`;
+		});
 	}
-    
-	this.coin.onload = function() {
-		imageLoaded();
-	}
-	this.coin.src = "assets/coin.png";
-    
-	this.hazards.forEach((h,i) => {
-        h.onload = () => imageLoaded();
-        h.src = `assets/hazzard-${i+1}.png`;
-    });
-    }
 }
